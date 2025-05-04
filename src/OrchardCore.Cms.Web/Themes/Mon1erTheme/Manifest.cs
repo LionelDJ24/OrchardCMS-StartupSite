96f19012608cc165a5d715f3515758ffa0ad72eb
LionelDJ24 <lionel.ndoizine04@gmail.com>
2025-05-04 11:50:11 +0200

    Initial commit - Orchard CMS project

---
+++ src/OrchardCore.Cms.Web/Themes/Mon1erTheme/Manifest.cs
@@ -0,0 +1,11 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Mon1erTheme",
+    Author = "Lionel",
+    Website = "https://monsite.local",
+    Version = "1.0.0",
+    Description = "Mon premier thème Orchard Core",
+    Tags = new[] { "Blog", "Custom", "Theme" },
+    BaseTheme = "TheTheme" 
+)]
---
+++ src/OrchardCore.Cms.Web/Themes/Mon1erTheme/Theme.cs
@@ -0,0 +1,8 @@
+using OrchardCore.DisplayManagement.Theming;
+
+namespace Mon1erTheme
+{
+    public class Theme : ThemeBase
+    {
+    }
+}
